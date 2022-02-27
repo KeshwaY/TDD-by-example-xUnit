9bb1a116a1ce49b4e53eb75bf7489cc6b9fffb55
Damian Ciepiela <dciepiela@yahoo.com>
2022-02-27 20:55:56 +0100

    feat: chapter 19

--- TestCase.py
+++ TestCase.py
@@ -2,6 +2,10 @@ class TestCase:
     def __init__(self, name) -> None:
         self.name = name
 
+    def setUp(self):
+        ...
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
--- WasRun.py
+++ WasRun.py
@@ -5,6 +5,10 @@ class WasRun(TestCase):
     def __init__(self, name) -> None:
         super(WasRun, self).__init__(name)
         self.wasRun = None
+        self.wasSetUp = None
 
     def testMethod(self):
         self.wasRun = 1
+
+    def setUp(self):
+        self.wasSetUp = 1
